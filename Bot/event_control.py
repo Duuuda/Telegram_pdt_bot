067a65630836c75bdd69998b4f1d4af93862a9c1
Duuuda <Duuuda@users.noreply.github.com>
2020-05-24 17:04:06 +0300

    Load project files

---
+++ Bot/event_control.py
@@ -0,0 +1,34 @@
+from time import sleep
+from datetime import datetime
+from Database import get_event, get_all_users, del_event
+from Unix import UnixTimeConverter
+
+
+unix_converter = UnixTimeConverter()
+
+
+def event_mainloop(bot):
+    try:
+        while True:
+            time = datetime.now()
+            unix = unix_converter(f'{time.hour}:{time.minute}')
+            weekday = time.strftime('%A').lower()
+            event = get_event(unix, weekday)
+            if event is not None:
+                all_users = get_all_users()
+                admins = tuple(filter(lambda user: user.is_admin, all_users))
+                pdts = tuple(filter(lambda user: user.is_pdt, all_users))
+                promotions = tuple(filter(lambda user: user.is_promotion, all_users))
+                if event.for_admins:
+                    for admin in admins:
+                        bot.forward_message(admin.user_id, event.chat_id, event.message_id)
+                if event.for_pdts:
+                    for pdt in pdts:
+                        bot.forward_message(pdt.user_id, event.chat_id, event.message_id)
+                if event.for_promotions:
+                    for promotion in promotions:
+                        bot.forward_message(promotion.user_id, event.chat_id, event.message_id)
+                del_event(unix, weekday)
+            sleep(60)
+    except Exception as error:
+        print(error)
